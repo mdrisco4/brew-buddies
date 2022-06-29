9e85257117ac4c2f262d56c9f1dc2a6777c29522
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-06-29 15:18:18 -0400

    setting up basic layouts, need pages, links and css overhaul

---
+++ src/components/home.js
@@ -0,0 +1,203 @@
+import React from "react"
+import styled from "styled-components"
+
+
+const SectionTitle = styled.div`
+  font-size: 50px;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  margin-bottom: 20px;
+  line-height: 50px;
+  @media (min-width: 800px) {
+  }
+  @media (min-width: 1150px) {
+    font-size: 60px;
+    margin-bottom: 25px;
+  }
+`
+
+const MissionStatement = styled.div`
+  font-size: 24px;
+  text-align: center;
+  color: rgb(154, 157, 159);
+  font-weight: bold;
+  margin-bottom: 15px;
+  @media (min-width: 800px) {
+    font-size: 30px;
+  }
+  @media (min-width: 1150px) {
+  }
+`
+
+const BoardMemberContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 12px;
+  @media (min-width: 650px) {
+    padding-left: 10px;
+  }
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+  @media (min-width: 800px) {
+  }
+  @media (min-width: 1150px) {
+  }
+`
+
+const BoardPhoto = styled.img`
+  height: auto;
+  width: 80%;
+  margin: 0 10% 0 10%;
+  @media (min-width: 768px) {
+    width: 20%;
+    margin: 0 0 0 0;
+  }
+  @media (min-width: 800px) {
+  }
+  @media (min-width: 1150px) {
+    /* height: 340px; */
+  }
+`
+
+const BoardTitle = styled.div`
+  font-size: 20px;
+  margin-left: 10px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  font-weight: bold;
+  line-height: 35px;
+  color: white;
+  text-decoration: none;
+  @media (min-width: 550px) {
+    font-size: 28px;
+  }
+  @media (min-width: 800px) {
+    font-size: 35px;
+  }
+  @media (min-width: 1150px) {
+    font-size: 40px;
+  }
+`
+
+const BoardDescription = styled.div`
+  font-size: 15px;
+  margin-left: 10px;
+  color: rgb(154, 157, 159);
+  font-weight: bold;
+  @media (min-width: 550px) {
+    font-size: 20px;
+  }
+  @media (min-width: 800px) {
+    font-size: 24px;
+  }
+  @media (min-width: 1150px) {
+    font-size: 32px;
+  }
+`
+
+const ContactInfo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  @media (min-width: 650px) {
+    text-align: left;
+  }
+  @media (min-width: 850px) {
+    font-size: 32px;
+    margin-top: 4px;
+  }
+  @media (min-width: 1150px) {
+    font-size: 45px;
+  }
+`
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media (min-width: 650px) {
+    flex-direction: row;
+  }
+  @media (min-width: 850px) {
+  }
+  @media (min-width: 1200px) {
+  }
+`
+
+const FooterLogo = styled.img`
+  @media (min-width: 650px) {
+    height: 100px;
+    width: 100px;
+    margin: 0;
+  }
+  @media (min-width: 850px) {
+    height: 140px;
+    width: 140px;
+  }
+  @media (min-width: 1150px) {
+    height: 180px;
+    width: 180px;
+  }
+`
+
+const Home = () => (
+  <div style={{ maxWidth: "1200px", marginLeft: "10%", marginRight: "10%" }}>
+    <SectionTitle>Welcome to Seeds of Support!</SectionTitle>
+    <MissionStatement>
+      Mission Statement
+    </MissionStatement>
+    <SectionTitle>Board of Directors</SectionTitle>
+    <BoardMemberContainer>
+      {/* <BoardPhoto src="https://i.imgur.com/RVGefKN.png" /> */}
+      <div>
+        {/* <Link to="/Jake/" style={{ textDecoration: 'none' }}>
+          <BoardTitle>Chief Executive Officer: Jake Sassaman</BoardTitle>
+        </Link> */}
+        <BoardDescription>
+        BrewBuddies was formed out of a desire to improve the craft beverage experience for breweries, bars, restaurants, and the consumers themselves. New York State allows craft beverage companies to sell their products directly to consumers and retailers without the need to partner with a traditional distributor, and our goal is to help those companies realize the maximum potential from this unique opportunity.
+        </BoardDescription>
+      </div>
+    </BoardMemberContainer>
+    <BoardMemberContainer>
+      {/* <BoardPhoto src="https://i.imgur.com/Qj8xvO8.jpg" /> */}
+      <div>
+        {/* <Link to="/Heather/" style={{ textDecoration: 'none' }}>
+          <BoardTitle>Chief Operating Officer: Heather Yount</BoardTitle>
+        </Link> */}
+        <BoardDescription>
+          More content
+        </BoardDescription>
+      </div>
+    </BoardMemberContainer>
+    <BoardMemberContainer>
+      <BoardPhoto src="https://i.imgur.com/rpLw26n.png" />
+      <div>
+        {/* <Link to="/Natalia/" style={{ textDecoration: 'none' }}>
+          <BoardTitle>Advisory Board Member: Natalia Thurston</BoardTitle>
+        </Link> */}
+        <BoardDescription>
+          Additional content
+        </BoardDescription>
+      </div>
+    </BoardMemberContainer>
+    {/* <Link to="/Contact/" style={{ textDecoration: "none", color: "white" }}>
+      <SectionTitle>Contact Us</SectionTitle>
+    </Link> */}
+    <FooterContainer>
+      {/* <div>
+        <ContactInfo>560 Thomas L. Berkley Way</ContactInfo>
+        <ContactInfo>Oakland, CA 94612</ContactInfo>
+        <ContactInfo>(510) 354-9872</ContactInfo>
+      </div> */}
+      {/* <FooterLogo
+        src="https://i.imgur.com/3ei96FN.png"
+        alt="logo"
+        className="logo"
+      /> */}
+    </FooterContainer>
+  </div>
+)
+
+export default Home;
