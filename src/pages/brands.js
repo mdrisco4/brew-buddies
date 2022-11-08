7c29fcf4aa2b2362a599777ddc6da3d4c3637fb5
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-11-08 14:23:20 -0500

    worked on some styling issues, primarily the arrangenent of the logos on the BRANDS page

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,40 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import "../styles/fonts.css"
+import "../styles/fonts.css";
 
-const BrandHeader = styled.h1`
-  font-family: 'Comfortaa', cursive;
+const BrandHeader = styled.div`
+  font-family: "Comfortaa", cursive;
   padding-top: 50px;
   text-decoration: underline;
+  font-size: 28px;
   @media (min-width: 480px) {
-
+    font-size: 32px;
   }
   @media (min-width: 576px) {
-
+    font-size: 35px;
   }
   @media (min-width: 650px) {
-    
+    font-size: 39px;
   }
   @media (min-width: 768px) {
-    
+    font-size: 44px;
   }
   @media (min-width: 870px) {
-    
+    font-size: 48px;
   }
   @media (min-width: 980px) {
-    
+    font-size: 54px;
   }
   @media (min-width: 1100px) {
-    
+    font-size: 60px;
   }
   @media (min-width: 1260px) {
-    
+    font-size: 66px;
   }
 `;
 
 const BrandInfo = styled.div`
   font-size: 24px;
-  font-family: 'Comfortaa', cursive;
+  font-family: "Comfortaa", cursive;
   text-align: center;
   /* color: rgb(154, 157, 159); */
   font-weight: bold;
@@ -44,64 +45,81 @@ const BrandInfo = styled.div`
   padding-top: 20px;
   padding-bottom: 30px;
   @media (min-width: 480px) {
-
-}
-@media (min-width: 576px) {
-
-}
-@media (min-width: 650px) {
-  
-}
-@media (min-width: 768px) {
-  
-}
-@media (min-width: 870px) {
-  
-}
-@media (min-width: 980px) {
-  
-}
-@media (min-width: 1100px) {
-  
-}
-@media (min-width: 1260px) {
-  
-}
+    font-size: 28px;
+  }
+  @media (min-width: 576px) {
+    font-size: 32px;
+  }
+  @media (min-width: 650px) {
+    font-size: 35px;
+  }
+  @media (min-width: 768px) {
+    font-size: 39px;
+  }
+  @media (min-width: 870px) {
+    font: 44px;
+  }
+  @media (min-width: 980px) {
+    font-size: 48px;
+  }
+  @media (min-width: 1100px) {
+    font-size: 54px;
+  }
+  @media (min-width: 1260px) {
+    font-size: 60px;
+  }
 `;
 
 const LogoContainer1 = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  width: 95%;
+  width: 70%;
   margin-left: auto;
   margin-right: auto;
   border-color: rgb(154, 157, 159);
   @media (min-width: 550px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    width: 95%;
+    grid-template-columns: 1fr 1fr;
+    width: 70%;
   }
   @media (min-width: 840px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
   }
 `;
 
 const LogoContainer2 = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   width: 95%;
   margin-left: auto;
   margin-right: auto;
   border-color: rgb(154, 157, 159);
   @media (min-width: 550px) {
     grid-template-columns: 1fr 1fr 1fr;
-    width: 70%;
+    width: 95%;
   }
   @media (min-width: 840px) {
     grid-template-columns: 1fr 1fr 1fr;
   }
 `;
 
-const BrandLink = styled.a``;
+const LogoContainer3 = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  border-color: rgb(154, 157, 159);
+  @media (min-width: 550px) {
+    grid-template-columns: 1fr 1fr;
+    width: 70%;
+  }
+  @media (min-width: 840px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+const BrandLink = styled.a`
+`;
 
 const Logo = styled.img`
   width: 50%;
@@ -115,7 +133,13 @@ const Logo = styled.img`
 `;
 
 const Brands = () => (
-  <div style={{ maxWidth: "15000px", backgroundColor: "rgb(135, 180, 225)", textAlign: "center" }}>
+  <div
+    style={{
+      maxWidth: "15000px",
+      backgroundColor: "rgb(135, 180, 225)",
+      textAlign: "center",
+    }}
+  >
     <BrandHeader>Featured Brands</BrandHeader>
     <BrandInfo>
       We strive to bring you the best in craft beer with our services. If you
@@ -128,24 +152,26 @@ const Brands = () => (
       <BrandLink href="http://rustynickelbrewing.com/" target="_blank">
         <Logo src="https://i.imgur.com/PUEJmsR.jpg" />
       </BrandLink>
+    </LogoContainer1>
+    <LogoContainer2>
       <BrandLink href="https://www.lillybellemeads.com/" target="_blank">
         <Logo src="https://i.imgur.com/NUr7F8O.png" />
       </BrandLink>
       <BrandLink href="https://www.butternutsbeerandale.com/" target="_blank">
         <Logo src="https://i.imgur.com/Vv4AcHe.png" />
       </BrandLink>
-      </LogoContainer1>
-      <LogoContainer2>
-      <BrandLink href="https://elifishbrewing.com/" target="blank">
-        <Logo src="https://i.imgur.com/Xns4WcX.png" />
-      </BrandLink>
       <BrandLink href="https://ninemaidensbrewing.com/" target="_blank">
         <Logo src="https://i.imgur.com/sPjMWdH.jpg" />
+      </BrandLink>
+    </LogoContainer2>
+    <LogoContainer3>
+      <BrandLink href="https://elifishbrewing.com/" target="blank">
+        <Logo src="https://i.imgur.com/Xns4WcX.png" />
       </BrandLink>
       <BrandLink href="https://www.westshorebrewing.com/" target="_blank">
         <Logo src="https://i.imgur.com/9JdZvY0.png" />
       </BrandLink>
-    </LogoContainer2>
+    </LogoContainer3>
   </div>
 );
 
