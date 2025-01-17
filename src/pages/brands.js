96cabf17bd93c4aef86b7aff82248752aa6dffed
michaeldriscoll <michaelndriscoll81@gmail.com>
2025-01-16 18:49:48 -0800

    resized the text on the brands page

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -6,36 +6,36 @@ const BrandHeader = styled.div`
   font-family: "Comfortaa", cursive;
   padding-top: 50px;
   text-decoration: underline;
-  font-size: 28px;
+  font-size: 24px;
   @media (min-width: 480px) {
-    font-size: 32px;
+    font-size: 28px;
   }
   @media (min-width: 576px) {
-    font-size: 35px;
+    font-size: 32px;
   }
   @media (min-width: 650px) {
-    font-size: 39px;
+    font-size: 33px;
   }
   @media (min-width: 768px) {
-    font-size: 44px;
+    font-size: 39px;
   }
   @media (min-width: 870px) {
-    font-size: 48px;
+    font-size: 42px;
   }
   @media (min-width: 980px) {
-    font-size: 54px;
+    font-size: 45px;
   }
   @media (min-width: 1100px) {
-    font-size: 60px;
+    font-size: 50px;
   }
   @media (min-width: 1260px) {
-    font-size: 66px;
+    font-size: 52px;
   }
 `;
 
 const BrandInfo = styled.div`
-  text-align: left;
-  font-size: 24px;
+  text-align: center;
+  font-size: 20px;
   font-family: "Comfortaa", cursive;
   font-weight: bold;
   margin-bottom: 15px;
@@ -44,29 +44,29 @@ const BrandInfo = styled.div`
   padding-top: 20px;
   padding-bottom: 30px;
   @media (min-width: 480px) {
-    font-size: 28px;
+    font-size: 24px;
   }
   @media (min-width: 576px) {
-    font-size: 32px;
+    font-size: 30px;
   }
   @media (min-width: 650px) {
-    font-size: 35px;
+    font-size: 30px;
   }
   @media (min-width: 768px) {
     text-align: center;
-    font-size: 39px;
+    font-size: 33px;
   }
   @media (min-width: 870px) {
-    font: 44px;
+    font: 36px;
   }
   @media (min-width: 980px) {
-    font-size: 48px;
+    font-size: 40px;
   }
   @media (min-width: 1100px) {
-    font-size: 54px;
+    font-size: 48px;
   }
   @media (min-width: 1260px) {
-    font-size: 60px;
+    font-size: 50px;
   }
 `;
 
@@ -121,10 +121,10 @@ const LogoContainer3 = styled.div`
 `;
 
 const BrandLink = styled.a`
-transition: transform .55s;
-:hover {
-  transform: scale(1.25);
-}
+  transition: transform 0.55s;
+  :hover {
+    transform: scale(1.25);
+  }
 `;
 
 const Logo = styled.img`
@@ -146,8 +146,9 @@ const Brands = () => (
   >
     <BrandHeader>Featured Brands</BrandHeader>
     <BrandInfo>
-      BrewBuddies is proud to represent the following brands. Click their logos
-      to learn more about each of our partners!
+      {/* BrewBuddies is proud to represent the following brands. Click their logos
+      to learn more about each of our partners! */}
+      Click their logos to learn more about each of the brands we work with!'
     </BrandInfo>
     <LogoContainer2>
       <BrandLink href="https://sagerbeerworks.com/" target="_blank">
@@ -161,7 +162,10 @@ const Brands = () => (
       </BrandLink>
     </LogoContainer2>
     <LogoContainer2>
-      <BrandLink href="https://www.stoneyardbrewingcompany.com/" target="_blank">
+      <BrandLink
+        href="https://www.stoneyardbrewingcompany.com/"
+        target="_blank"
+      >
         <Logo src="https://i.imgur.com/hpDcx39.png" />
       </BrandLink>
       <BrandLink href="https://ninemaidensbrewing.com/" target="_blank">
@@ -184,65 +188,71 @@ const Brands = () => (
     </LogoContainer2>
     <LogoContainer2>
       <BrandLink href="https://www.twinforkbeer.com/" target="_blank">
-      <Logo src="https://i.imgur.com/hj0ZqYt.png" />
+        <Logo src="https://i.imgur.com/hj0ZqYt.png" />
       </BrandLink>
       <BrandLink href="https://www.beelzebubbles.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/T0u0a1N.png" />
+        <Logo src="https://i.imgur.com/T0u0a1N.png" />
       </BrandLink>
       <BrandLink href="https://www.katboocha.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/ZhvEMfn.png" />
+        <Logo src="https://i.imgur.com/ZhvEMfn.png" />
       </BrandLink>
     </LogoContainer2>
     <LogoContainer2>
       <BrandLink href="https://www.luckyharebrewing.com/" target="_blank">
-      <Logo src="https://i.imgur.com/KJCE4FK.png" />
+        <Logo src="https://i.imgur.com/KJCE4FK.png" />
       </BrandLink>
-      <BrandLink href="https://happygutsanctuary.com/index.html" target="_blank">
-      <Logo  src="https://i.imgur.com/Y6wWNjt.png" />
+      <BrandLink
+        href="https://happygutsanctuary.com/index.html"
+        target="_blank"
+      >
+        <Logo src="https://i.imgur.com/Y6wWNjt.png" />
       </BrandLink>
       <BrandLink href="https://seedandstonecidery.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/42OnRME.png" />
+        <Logo src="https://i.imgur.com/42OnRME.png" />
       </BrandLink>
     </LogoContainer2>
     <LogoContainer2>
       <BrandLink href="https://www.tilbrewing.com/" target="_blank">
-      <Logo src="https://i.imgur.com/GyvBrIu.png" />
+        <Logo src="https://i.imgur.com/GyvBrIu.png" />
       </BrandLink>
-      <BrandLink href="https://www.facebook.com/steampunkcider/" target="_blank">
-      <Logo  src="https://i.imgur.com/tL5W19N.png" />
+      <BrandLink
+        href="https://www.facebook.com/steampunkcider/"
+        target="_blank"
+      >
+        <Logo src="https://i.imgur.com/tL5W19N.png" />
       </BrandLink>
       <BrandLink href="https://seedandstonecidery.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/jLQV4S3.gif" />
+        <Logo src="https://i.imgur.com/jLQV4S3.gif" />
       </BrandLink>
     </LogoContainer2>
     <LogoContainer2>
       <BrandLink href="https://bullfinchbrewpub.com/" target="_blank">
-      <Logo src="https://i.imgur.com/nRj22M5.png" />
+        <Logo src="https://i.imgur.com/nRj22M5.png" />
       </BrandLink>
       <BrandLink href="https://wagnerbrewing.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/YkLBZSh.png" />
+        <Logo src="https://i.imgur.com/YkLBZSh.png" />
       </BrandLink>
       <BrandLink href="https://brazenbrewco.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/raXHyWZ.png" />
+        <Logo src="https://i.imgur.com/raXHyWZ.png" />
       </BrandLink>
     </LogoContainer2>
     <LogoContainer2>
       <BrandLink href="https://www.brindlehausbrewing.com/" target="_blank">
-      <Logo src="https://i.imgur.com/GjQOJxj.png" />
+        <Logo src="https://i.imgur.com/GjQOJxj.png" />
       </BrandLink>
       <BrandLink href="https://buffalo-brewing-company.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/99eoAiw.png" />
+        <Logo src="https://i.imgur.com/99eoAiw.png" />
       </BrandLink>
       <BrandLink href="https://www.peacemakerbrewing.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/yK3kthB.png" />
+        <Logo src="https://i.imgur.com/yK3kthB.png" />
       </BrandLink>
     </LogoContainer2>
     <LogoContainer3>
       <BrandLink href="https://octopusbrew.com/" target="_blank">
-      <Logo src="https://i.imgur.com/i9fFQCZ.png" />
+        <Logo src="https://i.imgur.com/i9fFQCZ.png" />
       </BrandLink>
       <BrandLink href="https://www.stumblinmonkeybeer.com/" target="_blank">
-      <Logo  src="https://i.imgur.com/z5Wi4iB.png" />
+        <Logo src="https://i.imgur.com/z5Wi4iB.png" />
       </BrandLink>
     </LogoContainer3>
   </div>
